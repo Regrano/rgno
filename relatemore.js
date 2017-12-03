57b3a00742c85580c5a853d65839dca255bc84c6
Regrano <34178621+Regrano@users.noreply.github.com>
2017-12-04 02:31:00 +0700

    Create relatemore.js

---
+++ relatemore.js
@@ -0,0 +1,107 @@
+    var imageUrls1 = [
+    &quot;https://4.bp.blogspot.com/-BxfZPsbW404/V9uVa6QP1bI/AAAAAAAAAJ8/SSKFe54q3q8fkIN8wYoLHp76U_9q1SCRQCLcB/s210/read-fun-home-a-family-tragicomic.jpg&quot;
+    , &quot;https://2.bp.blogspot.com/-vpiq1ss8yik/WUEXCTJtUxI/AAAAAAAAGI4/412rZMHny8IOr5jRzsaR_9wT6ZSBSJVKwCLcBGAs/s210/read-marjane-satrapi-persepolis-1-graphic-novel.jpg&quot;
+	, &quot;https://3.bp.blogspot.com/-10ODm-9oAEM/WQbYvZbyqUI/AAAAAAAADQE/1iYKRXw4iAAPjISrYL7bXkvNzJYJQ981wCLcB/s210/read-naoki-urasawa-pluto-graphic-novel-manga.jpg&quot;
+	, &quot;https://2.bp.blogspot.com/-IazfOo90dko/WeQxwwAeL1I/AAAAAAAAG7A/2siR2LKvkGoqH7PA30BCyy1Zcak6HQ-dQCLcBGAs/s210/read-pride-of-baghdad-graphic-novel.jpg&quot;
+    ];  
+   var imageLinks1 = [
+	&quot;https://readgraphicnovels.blogspot.com/2016/09/read-fun-home-family-tragicomic-graphic-novel.html&quot;
+	, &quot;https://readgraphicnovels.blogspot.com/2017/06/read-marjane-satrapi-persepolis-1.html&quot;
+	, &quot;https://readgraphicnovels.blogspot.com/2017/05/read-naoki-urasawa-pluto-graphic-novel-manga.html&quot;
+	, &quot;https://readgraphicnovels.blogspot.com/2017/10/read-pride-of-baghdad-graphic-novel.html&quot;
+    ];
+  var imageAlts1 = [
+	&quot;Read Fun Home: A Family Tragicomic Online&quot;
+	, &quot;Read Persepolis 1 Graphic Novel Online&quot;
+	, &quot;Read Naoki Urasawa&#39;s Pluto Manga Online&quot;
+	, &quot;Read Pride of Baghdad Online&quot;
+    ];
+
+    var imageUrls2 = [
+	&quot;https://2.bp.blogspot.com/-YG1FShq6dpE/WTTl435gu-I/AAAAAAAAFuo/CMMbPkuG5DU-yrw_MiD6ih4XNrjgirc_ACLcB/s210/read-are-you-my-mother-graphic-novel.jpg&quot;
+	, &quot;https://4.bp.blogspot.com/-kMhrR5tOhho/WPWUmzD1f4I/AAAAAAAACE4/lPNR79Y1qPABJ9KiYUcED-g_64Se-8XZwCLcB/s210/read-craig-thompson-habibi-graphic-novel.jpg&quot;
+	, &quot;https://3.bp.blogspot.com/-CR_OR1Xojp4/V-fq6u-V0oI/AAAAAAAAALs/6pyNsr90kvIJpCBnvAZJKODT_kxcgbVCACLcB/s210/read-beautiful-darkness-graphic-novel.jpg&quot;
+	, &quot;https://4.bp.blogspot.com/-RtHeaF8Ni5g/WPBry9ERspI/AAAAAAAABvA/w2SOGTDtdokDzQaAycPy6zCZ1pzwURNVgCLcB/s210/read-understanding-comics-graphic-novel.jpg&quot;
+    ];  
+   var imageLinks2 = [
+	&quot;https://readgraphicnovels.blogspot.com/2017/06/read-alison-bechdel-are-you-my-mother-graphic-novel.html&quot;
+	, &quot;https://readgraphicnovels.blogspot.com/2017/04/read-craig-thompson-habibi-graphic-novel.html&quot;
+	, &quot;https://readgraphicnovels.blogspot.com/2016/09/beautiful-darkness-graphic-novel.html&quot;
+	, &quot;https://readgraphicnovels.blogspot.com/2017/04/read-understanding-comics-graphic-novel.html&quot;
+    ];
+    var imageAlts2 = [
+	&quot;Read Are You My Mother Graphic Novel Online&quot;
+	, &quot;Read Craig Thompson&#39;s Habibi Online&quot;
+	, &quot;Read Beautiful Darkness Graphic Novel Online&quot;
+	, &quot;Read Scott McCloud&#39;s Understanding Comics Online&quot;       
+    ];
+
+    var imageUrls3 = [
+	&quot;https://4.bp.blogspot.com/-vLS_fOvdGvc/WUdkRoGfkVI/AAAAAAAAGV4/NheW2WUZPpUzzj75BGO2OlEiNy1Zc5EEQCLcBGAs/s210/read-marjane-satrapi-persepolis-2-graphic-novel.jpg&quot;
+	, &quot;https://1.bp.blogspot.com/-SN08h1_E43o/V-P32uanJJI/AAAAAAAAALE/HGKs88bZKPQRXG1OtvvM5IX8B03vxaB3wCPcB/s210/craig-thompson-goodbye-chunky-rice.jpg&quot;
+	, &quot;https://2.bp.blogspot.com/-qfSTMUniJsI/WBh1eLVsMTI/AAAAAAAABZY/FSFQ8TgA7CQGLeNsEOYvq_2Um7n1sZuLgCLcB/s210/read-a-contract-with-god-graphic-novel.jpg&quot;
+	, &quot;https://2.bp.blogspot.com/-6uniPL7a9dc/V-IOaXAFMhI/AAAAAAAAAKk/g_hksbU5Za0iYI0HRNvAaKIqUHokkY6xwCLcB/s210/cover-of-bernie-wrightson-s-frankenstein.jpg&quot;
+    ];  
+   var imageLinks3 = [
+	&quot;https://readgraphicnovels.blogspot.com/2017/06/read-marjane-satrapi-persepolis-2.html&quot;
+	, &quot;https://readgraphicnovels.blogspot.com/2016/09/good-bye-chunky-rice-graphic-novel.html&quot;
+	, &quot;https://readgraphicnovels.blogspot.com/2016/11/read-a-contract-with-god-graphic-novel.html&quot;
+	, &quot;https://readgraphicnovels.blogspot.com/2016/09/bernie-wrightsons-frankenstein.html&quot;
+    ];
+    var imageAlts3 = [
+	&quot;Read Persepolis 2 Graphic Novel Online&quot;
+	, &quot;Read Goodbye Chunky Rice Graphic Novel Online&quot;
+	, &quot;Read A Contract with God Graphic Novel Online&quot;
+	, &quot;Read Bernie Wrightson&#39;s Frankenstein Online&quot;
+    ];
+
+    var imageUrls4 = [
+	&quot;https://1.bp.blogspot.com/-4240bb1cAPQ/Wf1cqooQt3I/AAAAAAAAHM0/pKwYoGathkwzBWMUpxae-W6Oy1qnBcmggCLcBGAs/s210/read-online-anya-s-ghost-graphic-novel.jpg&quot;
+	, &quot;https://1.bp.blogspot.com/-NC5ZxFkL3qo/WUqD1_4OpaI/AAAAAAAAGpE/iVsC2HjqGJoGGE17KtRF0BdKAihrfU3OgCLcBGAs/s210/read-online-coraline-graphic-novel.jpg&quot;
+	, &quot;https://3.bp.blogspot.com/-c_iU4Gc2cnU/V_-nhtxZgRI/AAAAAAAAAOM/CvFrB6x-EQYHRtlYw_iTl-nzwkDWNw5RQCLcB/s210/read-craig-thompson-blankets-graphic-novel.jpg&quot;
+	, &quot;https://3.bp.blogspot.com/-OJjH_HtMeC0/Wg7kGRAJ38I/AAAAAAAAHrw/wWJ47Rj5gq4PzFrft5nBjbrLrndCEUCZwCLcBGAs/s210/read-joe-sacco-palestine-online.jpg&quot;
+    ];  
+   var imageLinks4 = [
+	&quot;https://readgraphicnovels.blogspot.com/2017/11/read-anya-s-ghost-graphic-novel-online.html&quot;
+	, &quot;https://readgraphicnovels.blogspot.com/2017/06/read-coraline-graphic-novel.html&quot;
+	, &quot;https://readgraphicnovels.blogspot.com/2016/10/read-craig-thompson-blankets-graphic-novel.html&quot;
+	, &quot;https://readgraphicnovels.blogspot.com/2017/11/read-joe-sacco-palestine-online.html&quot;
+    ];
+    var imageAlts4 = [
+	&quot;Read Anya&#39;s Ghost Graphic Novel Online&quot;
+	, &quot;Read Coraline Graphic Novel Online&quot;
+	, &quot;Read Craig Thompson&#39;s Blankets Online&quot;
+	, &quot;Joe Sacco - Palestine Online&quot;
+    ];
+
+  function getImageHtmlCode1() {
+    var dataIndex = Math.floor(Math.random() * imageUrls1.length);
+    var img = &#39;&lt;a href=\&quot;&#39; + imageLinks1[dataIndex] + &#39;&quot;&gt;&lt;img src=&quot;&#39;;        
+    img += imageUrls1[dataIndex];
+    img += &#39;\&quot; alt=\&quot;&#39; + imageAlts1[dataIndex] + &#39;&quot;/&gt;&lt;/a&gt;&#39;;
+    return img;
+  }
+
+  function getImageHtmlCode2() {
+    var dataIndex = Math.floor(Math.random() * imageUrls2.length);
+    var img = &#39;&lt;a href=\&quot;&#39; + imageLinks2[dataIndex] + &#39;&quot;&gt;&lt;img src=&quot;&#39;;        
+    img += imageUrls2[dataIndex];
+    img += &#39;\&quot; alt=\&quot;&#39; + imageAlts2[dataIndex] + &#39;&quot;/&gt;&lt;/a&gt;&#39;;
+    return img;
+  }
+
+  function getImageHtmlCode3() {
+    var dataIndex = Math.floor(Math.random() * imageUrls3.length);
+    var img = &#39;&lt;a href=\&quot;&#39; + imageLinks3[dataIndex] + &#39;&quot;&gt;&lt;img src=&quot;&#39;;        
+    img += imageUrls3[dataIndex];
+    img += &#39;\&quot; alt=\&quot;&#39; + imageAlts3[dataIndex] + &#39;&quot;/&gt;&lt;/a&gt;&#39;;
+    return img;
+  }
+
+  function getImageHtmlCode4() {
+    var dataIndex = Math.floor(Math.random() * imageUrls4.length);
+    var img = &#39;&lt;a href=\&quot;&#39; + imageLinks4[dataIndex] + &#39;&quot;&gt;&lt;img src=&quot;&#39;;        
+    img += imageUrls4[dataIndex];
+    img += &#39;\&quot; alt=\&quot;&#39; + imageAlts4[dataIndex] + &#39;&quot;/&gt;&lt;/a&gt;&#39;;
+    return img;
+  }
