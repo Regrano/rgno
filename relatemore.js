e4d5d446dcfc50c1ba821ca26ca2b358cff39944
Regrano <34178621+Regrano@users.noreply.github.com>
2018-01-28 22:36:06 +0700

    Update relatemore.js

--- relatemore.js
+++ relatemore.js
@@ -1,3 +1,88 @@
-function getImageHtmlCode1(){var a=Math.floor(Math.random()*imageUrls1.length),o='<a href="'+imageLinks1[a]+'"><img src="';return o+=imageUrls1[a],o+='" alt="'+imageAlts1[a]+'"/></a>'}function getImageHtmlCode2(){var a=Math.floor(Math.random()*imageUrls2.length),o='<a href="'+imageLinks2[a]+'"><img src="';return o+=imageUrls2[a],o+='" alt="'+imageAlts2[a]+'"/></a>'}function getImageHtmlCode3(){var a=Math.floor(Math.random()*imageUrls3.length),o='<a href="'+imageLinks3[a]+'"><img src="';return o+=imageUrls3[a],o+='" alt="'+imageAlts3[a]+'"/></a>'}function getImageHtmlCode4(){var a=Math.floor(Math.random()*imageUrls4.length),o='<a href="'+imageLinks4[a]+'"><img src="';return o+=imageUrls4[a],o+='" alt="'+imageAlts4[a]+'"/></a>'}var imageUrls1=["https://4.bp.blogspot.com/-BxfZPsbW404/V9uVa6QP1bI/AAAAAAAAAJ8/SSKFe54q3q8fkIN8wYoLHp76U_9q1SCRQCLcB/s210/read-fun-home-a-family-tragicomic.jpg","https://2.bp.blogspot.com/-vpiq1ss8yik/WUEXCTJtUxI/AAAAAAAAGI4/412rZMHny8IOr5jRzsaR_9wT6ZSBSJVKwCLcBGAs/s210/read-marjane-satrapi-persepolis-1-graphic-novel.jpg","https://3.bp.blogspot.com/-10ODm-9oAEM/WQbYvZbyqUI/AAAAAAAADQE/1iYKRXw4iAAPjISrYL7bXkvNzJYJQ981wCLcB/s210/read-naoki-urasawa-pluto-graphic-novel-manga.jpg","https://2.bp.blogspot.com/-IazfOo90dko/WeQxwwAeL1I/AAAAAAAAG7A/2siR2LKvkGoqH7PA30BCyy1Zcak6HQ-dQCLcBGAs/s210/read-pride-of-baghdad-graphic-novel.jpg","https://2.bp.blogspot.com/-pjwdu-XwtO8/WimAU6P7clI/AAAAAAAAIkg/bU5wfhU90sc6eG0aEvzaX62-WjY4RyGiQCLcBGAs/s1600/i-kill-giants-comic-read-online-archive.jpg"],imageLinks1=["https://readgraphicnovels.blogspot.com/2016/09/read-fun-home-family-tragicomic-graphic-novel.html","https://readgraphicnovels.blogspot.com/2017/06/read-marjane-satrapi-persepolis-1.html","https://readgraphicnovels.blogspot.com/2017/05/read-naoki-urasawa-pluto-graphic-novel-manga.html","https://readgraphicnovels.blogspot.com/2017/10/read-pride-of-baghdad-graphic-novel.html","http://readgraphicnovels.blogspot.com/2017/12/i-kill-giants-comic-read-online.html"],imageAlts1=["Read Fun Home: A Family Tragicomic Online","Read Persepolis 1 Graphic Novel Online","Read Naoki Urasawa's Pluto Manga Online","Read Pride of Baghdad Online","I Kill Giants Comic Online"],imageUrls2=["https://3.bp.blogspot.com/-RGXaIwAaXvY/WjaoR7ipK_I/AAAAAAAAI3A/FDpzLTGrU0s2uhIL808KXHaebv6ZxxtGACLcBGAs/s1600/read-a-christmas-carol-graphic-novel.jpg","https://2.bp.blogspot.com/-YG1FShq6dpE/WTTl435gu-I/AAAAAAAAFuo/CMMbPkuG5DU-yrw_MiD6ih4XNrjgirc_ACLcB/s210/read-are-you-my-mother-graphic-novel.jpg","https://4.bp.blogspot.com/-kMhrR5tOhho/WPWUmzD1f4I/AAAAAAAACE4/lPNR79Y1qPABJ9KiYUcED-g_64Se-8XZwCLcB/s210/read-craig-thompson-habibi-graphic-novel.jpg","https://3.bp.blogspot.com/-CR_OR1Xojp4/V-fq6u-V0oI/AAAAAAAAALs/6pyNsr90kvIJpCBnvAZJKODT_kxcgbVCACLcB/s210/read-beautiful-darkness-graphic-novel.jpg","https://4.bp.blogspot.com/-RtHeaF8Ni5g/WPBry9ERspI/AAAAAAAABvA/w2SOGTDtdokDzQaAycPy6zCZ1pzwURNVgCLcB/s210/read-understanding-comics-graphic-novel.jpg"],imageLinks2=["https://readgraphicnovels.blogspot.com/2017/12/read-a-christmas-carol-graphic-novel.html","https://readgraphicnovels.blogspot.com/2017/06/read-alison-bechdel-are-you-my-mother-graphic-novel.html","https://readgraphicnovels.blogspot.com/2017/04/read-craig-thompson-habibi-graphic-novel.html","https://readgraphicnovels.blogspot.com/2016/09/beautiful-darkness-graphic-novel.html","https://readgraphicnovels.blogspot.com/2017/04/read-understanding-comics-graphic-novel.html"],imageAlts2=["Read A Christmas Carol Graphic Novel","Read Are You My Mother Graphic Novel Online","Read Craig Thompson's Habibi Online","Read Beautiful Darkness Graphic Novel Online","Read Scott McCloud's Understanding Comics Online"],imageUrls3=["https://4.bp.blogspot.com/-vLS_fOvdGvc/WUdkRoGfkVI/AAAAAAAAGV4/NheW2WUZPpUzzj75BGO2OlEiNy1Zc5EEQCLcBGAs/s210/read-marjane-satrapi-persepolis-2-graphic-novel.jpg","https://1.bp.blogspot.com/-SN08h1_E43o/V-P32uanJJI/AAAAAAAAALE/HGKs88bZKPQRXG1OtvvM5IX8B03vxaB3wCPcB/s210/craig-thompson-goodbye-chunky-rice.jpg","https://2.bp.blogspot.com/-qfSTMUniJsI/WBh1eLVsMTI/AAAAAAAABZY/FSFQ8TgA7CQGLeNsEOYvq_2Um7n1sZuLgCLcB/s210/read-a-contract-with-god-graphic-novel.jpg","https://2.bp.blogspot.com/-6uniPL7a9dc/V-IOaXAFMhI/AAAAAAAAAKk/g_hksbU5Za0iYI0HRNvAaKIqUHokkY6xwCLcB/s210/cover-of-bernie-wrightson-s-frankenstein.jpg"],imageLinks3=["https://readgraphicnovels.blogspot.com/2017/06/read-marjane-satrapi-persepolis-2.html","https://readgraphicnovels.blogspot.com/2016/09/good-bye-chunky-rice-graphic-novel.html","https://readgraphicnovels.blogspot.com/2016/11/read-a-contract-with-god-graphic-novel.html","https://readgraphicnovels.blogspot.com/2016/09/bernie-wrightsons-frankenstein.html"],imageAlts3=["Read Persepolis 2 Graphic Novel Online","Read Goodbye Chunky Rice Graphic Novel Online","Read A Contract with God Graphic Novel Online","Read Bernie Wrightson's Frankenstein Online"],imageUrls4=["https://1.bp.blogspot.com/-4240bb1cAPQ/Wf1cqooQt3I/AAAAAAAAHM0/pKwYoGathkwzBWMUpxae-W6Oy1qnBcmggCLcBGAs/s210/read-online-anya-s-ghost-graphic-novel.jpg","https://1.bp.blogspot.com/-NC5ZxFkL3qo/WUqD1_4OpaI/AAAAAAAAGpE/iVsC2HjqGJoGGE17KtRF0BdKAihrfU3OgCLcBGAs/s210/read-online-coraline-graphic-novel.jpg","https://3.bp.blogspot.com/-c_iU4Gc2cnU/V_-nhtxZgRI/AAAAAAAAAOM/CvFrB6x-EQYHRtlYw_iTl-nzwkDWNw5RQCLcB/s210/read-craig-thompson-blankets-graphic-novel.jpg","https://3.bp.blogspot.com/-OJjH_HtMeC0/Wg7kGRAJ38I/AAAAAAAAHrw/wWJ47Rj5gq4PzFrft5nBjbrLrndCEUCZwCLcBGAs/s210/read-joe-sacco-palestine-online.jpg"],imageLinks4=["https://readgraphicnovels.blogspot.com/2017/11/read-anya-s-ghost-graphic-novel-online.html","https://readgraphicnovels.blogspot.com/2017/06/read-coraline-graphic-novel.html","https://readgraphicnovels.blogspot.com/2016/10/read-craig-thompson-blankets-graphic-novel.html","https://readgraphicnovels.blogspot.com/2017/11/read-joe-sacco-palestine-online.html"],imageAlts4=["Read Anya's Ghost Graphic Novel Online","Read Coraline Graphic Novel Online","Read Craig Thompson's Blankets Online","Joe Sacco - Palestine Online"];
+function getImageHtmlCode1(){var a=Math.floor(Math.random()*imageUrls1.length),o='<a href="'+imageLinks1[a]+'"><img src="';return o+=imageUrls1[a],o+='" alt="'+imageAlts1[a]+'"/></a>'}
+function getImageHtmlCode2(){var a=Math.floor(Math.random()*imageUrls2.length),o='<a href="'+imageLinks2[a]+'"><img src="';return o+=imageUrls2[a],o+='" alt="'+imageAlts2[a]+'"/></a>'}
+function getImageHtmlCode3(){var a=Math.floor(Math.random()*imageUrls3.length),o='<a href="'+imageLinks3[a]+'"><img src="';return o+=imageUrls3[a],o+='" alt="'+imageAlts3[a]+'"/></a>'}
+function getImageHtmlCode4(){var a=Math.floor(Math.random()*imageUrls4.length),o='<a href="'+imageLinks4[a]+'"><img src="';return o+=imageUrls4[a],o+='" alt="'+imageAlts4[a]+'"/></a>'}
+
+var imageUrls1=[
+"https://4.bp.blogspot.com/-BxfZPsbW404/V9uVa6QP1bI/AAAAAAAAAJ8/SSKFe54q3q8fkIN8wYoLHp76U_9q1SCRQCLcB/s210/read-fun-home-a-family-tragicomic.jpg"
+,"https://2.bp.blogspot.com/-vpiq1ss8yik/WUEXCTJtUxI/AAAAAAAAGI4/412rZMHny8IOr5jRzsaR_9wT6ZSBSJVKwCLcBGAs/s210/read-marjane-satrapi-persepolis-1-graphic-novel.jpg"
+,"https://3.bp.blogspot.com/-10ODm-9oAEM/WQbYvZbyqUI/AAAAAAAADQE/1iYKRXw4iAAPjISrYL7bXkvNzJYJQ981wCLcB/s210/read-naoki-urasawa-pluto-graphic-novel-manga.jpg"
+,"https://2.bp.blogspot.com/-IazfOo90dko/WeQxwwAeL1I/AAAAAAAAG7A/2siR2LKvkGoqH7PA30BCyy1Zcak6HQ-dQCLcBGAs/s210/read-pride-of-baghdad-graphic-novel.jpg"
+,"https://2.bp.blogspot.com/-pjwdu-XwtO8/WimAU6P7clI/AAAAAAAAIkg/bU5wfhU90sc6eG0aEvzaX62-WjY4RyGiQCLcBGAs/s1600/i-kill-giants-comic-read-online-archive.jpg"]
+
+,imageLinks1=[
+"https://readgraphicnovels.blogspot.com/2016/09/read-fun-home-family-tragicomic-graphic-novel.html"
+,"https://readgraphicnovels.blogspot.com/2017/06/read-marjane-satrapi-persepolis-1.html"
+,"https://readgraphicnovels.blogspot.com/2017/05/read-naoki-urasawa-pluto-graphic-novel-manga.html"
+,"https://readgraphicnovels.blogspot.com/2017/10/read-pride-of-baghdad-graphic-novel.html"
+,"http://readgraphicnovels.blogspot.com/2017/12/i-kill-giants-comic-read-online.html"]
+
+,imageAlts1=[
+"Read Fun Home: A Family Tragicomic Online"
+,"Read Persepolis 1 Graphic Novel Online"
+,"Read Naoki Urasawa's Pluto Manga Online"
+,"Read Pride of Baghdad Online"
+,"I Kill Giants Comic Online"]
+
+
+,imageUrls2=[
+"https://3.bp.blogspot.com/-RGXaIwAaXvY/WjaoR7ipK_I/AAAAAAAAI3A/FDpzLTGrU0s2uhIL808KXHaebv6ZxxtGACLcBGAs/s1600/read-a-christmas-carol-graphic-novel.jpg"
+,"https://2.bp.blogspot.com/-YG1FShq6dpE/WTTl435gu-I/AAAAAAAAFuo/CMMbPkuG5DU-yrw_MiD6ih4XNrjgirc_ACLcB/s210/read-are-you-my-mother-graphic-novel.jpg"
+,"https://4.bp.blogspot.com/-kMhrR5tOhho/WPWUmzD1f4I/AAAAAAAACE4/lPNR79Y1qPABJ9KiYUcED-g_64Se-8XZwCLcB/s210/read-craig-thompson-habibi-graphic-novel.jpg"
+,"https://3.bp.blogspot.com/-CR_OR1Xojp4/V-fq6u-V0oI/AAAAAAAAALs/6pyNsr90kvIJpCBnvAZJKODT_kxcgbVCACLcB/s210/read-beautiful-darkness-graphic-novel.jpg"
+,"https://4.bp.blogspot.com/-RtHeaF8Ni5g/WPBry9ERspI/AAAAAAAABvA/w2SOGTDtdokDzQaAycPy6zCZ1pzwURNVgCLcB/s210/read-understanding-comics-graphic-novel.jpg"]
+
+,imageLinks2=[
+"https://readgraphicnovels.blogspot.com/2017/12/read-a-christmas-carol-graphic-novel.html"
+,"https://readgraphicnovels.blogspot.com/2017/06/read-alison-bechdel-are-you-my-mother-graphic-novel.html"
+,"https://readgraphicnovels.blogspot.com/2017/04/read-craig-thompson-habibi-graphic-novel.html"
+,"https://readgraphicnovels.blogspot.com/2016/09/beautiful-darkness-graphic-novel.html"
+,"https://readgraphicnovels.blogspot.com/2017/04/read-understanding-comics-graphic-novel.html"]
+
+,imageAlts2=[
+"Read A Christmas Carol Graphic Novel"
+,"Read Are You My Mother Graphic Novel Online"
+,"Read Craig Thompson's Habibi Online"
+,"Read Beautiful Darkness Graphic Novel Online"
+,"Read Scott McCloud's Understanding Comics Online"]
+
+,imageUrls3=[
+"https://2.bp.blogspot.com/-xZeDR61kuuE/WkIcGLKX4sI/AAAAAAAAJB8/5cucPUsdVxQjlcD2617Fy2JU4l8pWCC9gCLcBGAs/s210/read-american-born-chinese-book-online.jpg"
+,"https://4.bp.blogspot.com/-vLS_fOvdGvc/WUdkRoGfkVI/AAAAAAAAGV4/NheW2WUZPpUzzj75BGO2OlEiNy1Zc5EEQCLcBGAs/s210/read-marjane-satrapi-persepolis-2-graphic-novel.jpg"
+,"https://1.bp.blogspot.com/-SN08h1_E43o/V-P32uanJJI/AAAAAAAAALE/HGKs88bZKPQRXG1OtvvM5IX8B03vxaB3wCPcB/s210/craig-thompson-goodbye-chunky-rice.jpg"
+,"https://2.bp.blogspot.com/-qfSTMUniJsI/WBh1eLVsMTI/AAAAAAAABZY/FSFQ8TgA7CQGLeNsEOYvq_2Um7n1sZuLgCLcB/s210/read-a-contract-with-god-graphic-novel.jpg"
+,"https://2.bp.blogspot.com/-6uniPL7a9dc/V-IOaXAFMhI/AAAAAAAAAKk/g_hksbU5Za0iYI0HRNvAaKIqUHokkY6xwCLcB/s210/cover-of-bernie-wrightson-s-frankenstein.jpg"]
+
+,imageLinks3=[
+"https://readgraphicnovels.blogspot.com/2017/12/read-american-born-chinese-book-online.html"
+,"https://readgraphicnovels.blogspot.com/2017/06/read-marjane-satrapi-persepolis-2.html"
+,"https://readgraphicnovels.blogspot.com/2016/09/good-bye-chunky-rice-graphic-novel.html"
+,"https://readgraphicnovels.blogspot.com/2016/11/read-a-contract-with-god-graphic-novel.html"
+,"https://readgraphicnovels.blogspot.com/2016/09/bernie-wrightsons-frankenstein.html"]
+
+,imageAlts3=[
+"Read American Born Chinese"
+,"Read Persepolis 2 Graphic Novel Online"
+,"Read Goodbye Chunky Rice Graphic Novel Online"
+,"Read A Contract with God Graphic Novel Online"
+,"Read Bernie Wrightson's Frankenstein Online"]
+
+,imageUrls4=[
+"https://1.bp.blogspot.com/-4240bb1cAPQ/Wf1cqooQt3I/AAAAAAAAHM0/pKwYoGathkwzBWMUpxae-W6Oy1qnBcmggCLcBGAs/s210/read-online-anya-s-ghost-graphic-novel.jpg"
+,"https://1.bp.blogspot.com/-NC5ZxFkL3qo/WUqD1_4OpaI/AAAAAAAAGpE/iVsC2HjqGJoGGE17KtRF0BdKAihrfU3OgCLcBGAs/s210/read-online-coraline-graphic-novel.jpg"
+,"https://3.bp.blogspot.com/-c_iU4Gc2cnU/V_-nhtxZgRI/AAAAAAAAAOM/CvFrB6x-EQYHRtlYw_iTl-nzwkDWNw5RQCLcB/s210/read-craig-thompson-blankets-graphic-novel.jpg"
+,"https://3.bp.blogspot.com/-OJjH_HtMeC0/Wg7kGRAJ38I/AAAAAAAAHrw/wWJ47Rj5gq4PzFrft5nBjbrLrndCEUCZwCLcBGAs/s210/read-joe-sacco-palestine-online.jpg"]
+
+,imageLinks4=[
+"https://readgraphicnovels.blogspot.com/2017/11/read-anya-s-ghost-graphic-novel-online.html"
+,"https://readgraphicnovels.blogspot.com/2017/06/read-coraline-graphic-novel.html"
+,"https://readgraphicnovels.blogspot.com/2016/10/read-craig-thompson-blankets-graphic-novel.html"
+,"https://readgraphicnovels.blogspot.com/2017/11/read-joe-sacco-palestine-online.html"]
+
+,imageAlts4=[
+"Read Anya's Ghost Graphic Novel Online"
+,"Read Coraline Graphic Novel Online"
+,"Read Craig Thompson's Blankets Online"
+,"Joe Sacco - Palestine Online"];
 
 function myFunction(){document.getElementById("myDropdown1").classList.toggle("show")}function myFunction2(){document.getElementById("myDropdown2").classList.toggle("show")}function myFunction3(){document.getElementById("myDropdown3").classList.toggle("show")}function myFunction4(){document.getElementById("myDropdown4").classList.toggle("show")}window.onclick=function(t){if(!t.target.matches(".dropbtn")){n=document.getElementsByClassName("dropdown-content");for(o=0;o<n.length;o++)(s=n[o]).classList.contains("show")&&s.classList.remove("show")}if(!t.target.matches(".dropbtn3")){n=document.getElementsByClassName("dropdown3-content");for(o=0;o<n.length;o++)(s=n[o]).classList.contains("show")&&s.classList.remove("show")}if(!t.target.matches(".dropbtn4")){var o,n=document.getElementsByClassName("dropdown4-content");for(o=0;o<n.length;o++){var s=n[o];s.classList.contains("show")&&s.classList.remove("show")}}};
